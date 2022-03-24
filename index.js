02197f67ce2e8573c1ccec17362a8013512bf459
Long Nguyen <>
2022-03-25 00:37:21 +0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import xlsx from 'node-xlsx';
 import path, { join } from "path";
 import fs from 'fs';
+import { takePhoneNumber, fixSoDienThoai, onExportFile } from "./src/take_phone_number.js";
 
 const __dirname = path.resolve();
 const fs2 = fs.promises;
@@ -11,6 +12,7 @@ const TYPE = {
 	FIX_SO_DIEN_THOAI: 0, //Sửa lỗi số điện thoại
 	FIX_KHONG_NHAY_DONG: 1, // Sửa lỗi toàn bộ dữ liệu trong 1 ô
 	FIX_SAN_PHAM: 2, //Sửa lỗi sản phẩm nhảy xuống các dòng trắng bên dưới
+	TAKE_PHONE_NUMBER: 3, //Lấy số điện thoại từ text
 }
 
 /* ---------------------- NHAP DU LIEU TU DAY ---------------------- */
@@ -21,132 +23,132 @@ const __DATA = `
 
 //const type = TYPE.FIX_SO_DIEN_THOAI
 // const type = TYPE.FIX_KHONG_NHAY_DONG
-const type = TYPE.FIX_SAN_PHAM;
-
+// const type = TYPE.FIX_SAN_PHAM;
+const type = TYPE.TAKE_PHONE_NUMBER;
 
 /* ------------------------------------------------------------------ */
 
-const onExportFile = async (arrText) => {
+// const onExportFile = async (arrText) => {
 
-	const options = {'!cols': [{ wch: 15 }, { wch: 30 }, { wch: 40 }, { wch: 10 }, { wch: 15 } ]};
+// 	const options = {'!cols': [{ wch: 15 }, { wch: 30 }, { wch: 40 }, { wch: 10 }, { wch: 15 } ]};
 
-	let buffer = xlsx.build([{name: "sheet1", data: arrText}], options); // Returns a buffer
+// 	let buffer = xlsx.build([{name: "sheet1", data: arrText}], options); // Returns a buffer
 
-	const pathFile = path.join(__dirname, 'files', 'exported_data.xlsx');
+// 	const pathFile = path.join(__dirname, 'files', 'exported_data.xlsx');
 
-	try {
-		await fs2.writeFile(pathFile, buffer);
-	} catch (err) {
-		return console.log(err);
-	}
+// 	try {
+// 		await fs2.writeFile(pathFile, buffer);
+// 	} catch (err) {
+// 		return console.log(err);
+// 	}
 
-    console.log("Xuất file thành công!!!");
-}
+//     console.log("Xuất file thành công!!!");
+// }
 
-const fixSoDienThoai = (text) => {
+// const fixSoDienThoai = (text) => {
 
-	let dataFixed = "";
+// 	let dataFixed = "";
 	
-	dataFixed = text.replace(/đ/g, "");
-	dataFixed = dataFixed.replace(/st:/g, "");
-	dataFixed = dataFixed.replace(/St:/g, "");
-	dataFixed = dataFixed.replace(/\./g, "");
-	dataFixed = dataFixed.replace(/\,/g, "");
-	dataFixed = dataFixed.replace(/ /g, "");
-	dataFixed = dataFixed.replace(/-/g, "");
-	dataFixed = dataFixed.replace(/St/g, "");
-	dataFixed = dataFixed.replace(/\*/g, "");
+// 	dataFixed = text.replace(/đ/g, "");
+// 	dataFixed = dataFixed.replace(/st:/g, "");
+// 	dataFixed = dataFixed.replace(/St:/g, "");
+// 	dataFixed = dataFixed.replace(/\./g, "");
+// 	dataFixed = dataFixed.replace(/\,/g, "");
+// 	dataFixed = dataFixed.replace(/ /g, "");
+// 	dataFixed = dataFixed.replace(/-/g, "");
+// 	dataFixed = dataFixed.replace(/St/g, "");
+// 	dataFixed = dataFixed.replace(/\*/g, "");
 	
-	dataFixed = dataFixed.replace(/sốn/g, "");
-	dataFixed = dataFixed.replace(/h0ặc/g, "");
-	dataFixed = dataFixed.replace(/D14:D/g, "");
+// 	dataFixed = dataFixed.replace(/sốn/g, "");
+// 	dataFixed = dataFixed.replace(/h0ặc/g, "");
+// 	dataFixed = dataFixed.replace(/D14:D/g, "");
 
-	if (dataFixed.startsWith('\`')) {
-		dataFixed = dataFixed.replace('\`', "");
-	}
+// 	if (dataFixed.startsWith('\`')) {
+// 		dataFixed = dataFixed.replace('\`', "");
+// 	}
 
-	if (dataFixed.startsWith('\'')) {
-		dataFixed = dataFixed.replace("\'", "");
-	}
+// 	if (dataFixed.startsWith('\'')) {
+// 		dataFixed = dataFixed.replace("\'", "");
+// 	}
 
-	if (dataFixed.startsWith('0:')) {
-		dataFixed = dataFixed.replace("0:", "");
-	}
+// 	if (dataFixed.startsWith('0:')) {
+// 		dataFixed = dataFixed.replace("0:", "");
+// 	}
 
-	const arrDataFixed = dataFixed.split('/');
+// 	const arrDataFixed = dataFixed.split('/');
 
-	if (arrDataFixed.length == 2) {
-		dataFixed = arrDataFixed[0];
-	}
+// 	if (arrDataFixed.length == 2) {
+// 		dataFixed = arrDataFixed[0];
+// 	}
 
-	const arrDataFixed1 = dataFixed.split('or');
+// 	const arrDataFixed1 = dataFixed.split('or');
 
-	if (arrDataFixed1.length == 2) {
-		dataFixed = arrDataFixed1[0];
-	}
+// 	if (arrDataFixed1.length == 2) {
+// 		dataFixed = arrDataFixed1[0];
+// 	}
 
-	const arrDataFixed2 = dataFixed.split('và');
+// 	const arrDataFixed2 = dataFixed.split('và');
 
-	if (arrDataFixed2.length == 2) {
-		dataFixed = arrDataFixed2[0];
-	}
+// 	if (arrDataFixed2.length == 2) {
+// 		dataFixed = arrDataFixed2[0];
+// 	}
 
-	const arrDataFixed3 = dataFixed.split('(');
+// 	const arrDataFixed3 = dataFixed.split('(');
 
-	if (arrDataFixed3.length == 2) {
-		dataFixed = arrDataFixed3[0];
-	}
+// 	if (arrDataFixed3.length == 2) {
+// 		dataFixed = arrDataFixed3[0];
+// 	}
 
-	const arrDataFixed4 = dataFixed.split('hoặc');
+// 	const arrDataFixed4 = dataFixed.split('hoặc');
 
-	if (arrDataFixed4.length == 2) {
-		dataFixed = arrDataFixed4[0];
-	}
+// 	if (arrDataFixed4.length == 2) {
+// 		dataFixed = arrDataFixed4[0];
+// 	}
 
-	const arrDataFixed5 = dataFixed.split('Hoặc');
+// 	const arrDataFixed5 = dataFixed.split('Hoặc');
 
-	if (arrDataFixed5.length == 2) {
-		dataFixed = arrDataFixed5[0];
-	}
+// 	if (arrDataFixed5.length == 2) {
+// 		dataFixed = arrDataFixed5[0];
+// 	}
 
-	const arrDataFixed6 = dataFixed.split('HOẶC');
+// 	const arrDataFixed6 = dataFixed.split('HOẶC');
 
-	if (arrDataFixed6.length == 2) {
-		dataFixed = arrDataFixed6[0];
-	}
+// 	if (arrDataFixed6.length == 2) {
+// 		dataFixed = arrDataFixed6[0];
+// 	}
 
-	const arrDataFixed7 = dataFixed.split('vs');
+// 	const arrDataFixed7 = dataFixed.split('vs');
 
-	if (arrDataFixed7.length == 2) {
-		dataFixed = arrDataFixed7[0];
-	}
+// 	if (arrDataFixed7.length == 2) {
+// 		dataFixed = arrDataFixed7[0];
+// 	}
 
-	const arrDataFixed8 = dataFixed.split('D');
+// 	const arrDataFixed8 = dataFixed.split('D');
 
-	if (arrDataFixed8.length == 2) {
-		dataFixed = arrDataFixed8[0];
-	}
+// 	if (arrDataFixed8.length == 2) {
+// 		dataFixed = arrDataFixed8[0];
+// 	}
 
-	if (dataFixed.startsWith('84')) {
-		dataFixed = dataFixed.replace("84", "0");
-	}
+// 	if (dataFixed.startsWith('84')) {
+// 		dataFixed = dataFixed.replace("84", "0");
+// 	}
 	
-	if (dataFixed.startsWith('O')) {
-		dataFixed = dataFixed.replace("O", "0");
-	}
+// 	if (dataFixed.startsWith('O')) {
+// 		dataFixed = dataFixed.replace("O", "0");
+// 	}
 
-	dataFixed = dataFixed.replace(/\//g, "");
-	dataFixed = dataFixed.replace(/\‘/g, "");
+// 	dataFixed = dataFixed.replace(/\//g, "");
+// 	dataFixed = dataFixed.replace(/\‘/g, "");
 
-	//END add 0
-	if (dataFixed.startsWith('9') || dataFixed.startsWith('8') || dataFixed.startsWith('7') || dataFixed.startsWith('3')) {
-		dataFixed = "0" + dataFixed
-	}
+// 	//END add 0
+// 	if (dataFixed.startsWith('9') || dataFixed.startsWith('8') || dataFixed.startsWith('7') || dataFixed.startsWith('3')) {
+// 		dataFixed = "0" + dataFixed
+// 	}
 
-	// console.log(dataFixed);
+// 	// console.log(dataFixed);
 
-	return dataFixed;
-}
+// 	return dataFixed;
+// }
 
 const fixKhongNhayDong = () => {
 	let arrText = __DATA.split('\n');
@@ -253,11 +255,12 @@ const fixSanPham = () => {
     onExportFile(tempData);
 }
 
-
 if (type === TYPE.FIX_SO_DIEN_THOAI) {
 	onStartFixPhoneNumber();
 } else if (type === TYPE.FIX_SO_DIEN_THOAI) {
 	fixKhongNhayDong();
 } else if (type === TYPE.FIX_SAN_PHAM) {
 	fixSanPham();
+} else if (type === TYPE.TAKE_PHONE_NUMBER) {
+	takePhoneNumber();
 }
